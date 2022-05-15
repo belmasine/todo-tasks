e7690eee97f7dad46f37d9921fa4cd5c39d7f44f
Yassine Belmahfoud <yassinebelmahfoud@gmail.com>
2022-05-16 01:19:12 +0200

    feat: use wrapper for refactoring code controller

--- server-task/controllers/tasks.js
+++ server-task/controllers/tasks.js
@@ -1,59 +1,51 @@
 const Task = require('../models/Task');
+const asyncWrap = require('../middleware/async');
 
 const getTasks = async(req,res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks});
 }
 
-const postTask = async (req,res) => {
+/*const postTask = async (req,res) => {
   try {
       const task = await Task.create(req.body);
       res.status(201).json({task});
   } catch (error) {
       res.status(500).json({msg:  error});
   }
-}
+}*/
 
-const getTask = async(req,res) => {
-  try {
+const postTask = asyncWrap(async (req,res) => {
+      const task = await Task.create(req.body);
+      res.status(201).json({task});
+})
+
+const getTask = asyncWrap(async(req,res) => {
     const _id = req.params.id;
     const task = await Task.findOne({ _id }).exec();
     if (!task) {
         return res.status(404).json({ msg: `Id ${_id} not found` });
     }
     res.status(200).json({task}); 
-  } catch (error) {
-      res.status(500).json({ msg: error });
-  }  
-}
+})
 
-const updateTask = async (req,res) => {
-  try {
-    const { id: taskId } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body);
-    if (!task) {
-      return  res.status(404).json({msg: `Id  ${taskId} not found`});
-    }
-
-    res.status(200).json(task);
-
-  } catch (error) {
-    res.status(500).json({msg: error});
+const updateTask = asyncWrap(async (req,res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskId }, req.body);
+  if (!task) {
+    return  res.status(404).json({msg: `Id  ${taskId} not found`});
   }
-}
+  res.status(200).json(task);
+});
 
-const deleteTask = async (req,res) => {
-    try {
-        const { id: taskId } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskId }).exec();
-        if (!task) {
-            return  res.status(404).json({msg: `Id  ${taskId} not found`});
-        }
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({msg: error});
-    }
-}
+const deleteTask = asyncWrap(async (req,res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskId }).exec();
+  if (!task) {
+      return  res.status(404).json({msg: `Id  ${taskId} not found`});
+  }
+  res.status(200).json(task);
+})
 
 module.exports = {
     getTasks,
